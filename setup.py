5eb9ced898670742fd542e482741ee63b5b08e5a
kkevinmartinezz <kevin425.m@hotmail.com>
2025-04-14 16:56:23 -0400

    edited repo

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools.config.expand import find_packages
 setup(
   name = 'pygraspi_test',         # How you named your package folder (MyLib)
   packages = find_packages(),   # Chose the same as "name"
-  version = '0.1.8',      # Start with a small number and increase it with every change you make
+  version = '0.1.9',      # Start with a small number and increase it with every change you make
   license='Apache 2.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Testing out how setting up a pypi package is done correctly',   # Give a short description about your library
   author = 'Kevin E. Martinez Sanchez',                   # Type in your name
   author_email = 'kevin425.m@hotmail.com',      # Type in your E-Mail
   url = 'https://github.com/kkevinmartinezz/PyTest',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/kkevinmartinezz/PyTest/archive/refs/tags/v_01.7.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/kkevinmartinezz/PyTest/archive/refs/tags/v_01.8.tar.gz',    # I explain this later on
   keywords = ['testing', 'pygraspi', 'example'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'notebook',
